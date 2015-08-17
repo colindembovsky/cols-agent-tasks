dd8e23bfa2ad30c5912956604b540f9ff135f238
Colin Dembovsky <colindembovsky@gmail.com>
2015-08-17 19:11:09 +0200

    Adding initial node implementation

---
+++ typings/tsd.d.ts
@@ -0,0 +1,8 @@
+/// <reference path="q/Q.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="vso-task-lib/vso-task-lib.d.ts" />
+/// <reference path="shelljs/shelljs.d.ts" />
+
+declare module "vso-task-lib" {
+	export = VsoTaskLib;
+}

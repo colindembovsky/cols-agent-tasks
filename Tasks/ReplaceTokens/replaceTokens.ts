9ed53756c8b38714a36d2518f7586caec4cb0500
Peter Malik <peter.malik@outlook.com>
2016-10-11 23:39:51 +0200

    - Add function to collect all files from 'sourcePath' and its SubDirs - Add bool variable into task definition to [enable | disable] new feature

--- Tasks/ReplaceTokens/replaceTokens.ts
+++ Tasks/ReplaceTokens/replaceTokens.ts
@@ -2,6 +2,25 @@ import * as tl from 'vsts-task-lib/task';
 import * as sh from 'shelljs';
 import * as fs from 'fs';
 import * as os from 'os';
+import * as path from 'path';
+
+// function to recursivelly search all files starting from path stored in variable: sourcePath
+function GetAllFilesSync(dir, filelist) {
+    var fs = fs || require('fs'), files = fs.readdirSync(dir);
+    filelist = filelist || [];
+    files.forEach(function (file) {
+        var joinedFileWithPath = path.join(dir, file);
+        if (fs.statSync(joinedFileWithPath).isDirectory()) {
+            console.info(`Found directory: [${joinedFileWithPath}]`);
+            filelist = GetAllFilesSync(joinedFileWithPath, filelist);
+        }
+        else {
+            console.info(`Found file: [${joinedFileWithPath}]`);
+            filelist.push(joinedFileWithPath);
+        }
+    });
+    return filelist;
+}
 
 async function run() {
     try {
@@ -15,29 +34,31 @@ async function run() {
         var filePattern = tl.getInput("filePattern", true);
         var tokenRegex = tl.getInput("tokenRegex", true);
         var secretTokenInput = tl.getInput("secretTokens", false);
+        var useFindMethod = tl.getBoolInput("useFindMethod", true);
 
         // store the tokens and values if there is any secret token input 
-        var secretTokens: {[id: string]: string} = {};
+        var secretTokens: { [id: string]: string } = {};
         if (secretTokenInput != null && typeof secretTokenInput !== 'undefined') {
-            var inputArray : string[] = secretTokenInput.split(";");
+            var inputArray: string[] = secretTokenInput.split(";");
             for (var token of inputArray) {
-                if (token.indexOf(":") > -1) {  
-                    var valArray : string[] = token.split(":");
+                if (token.indexOf(":") > -1) {
+                    var valArray: string[] = token.split(":");
                     if (valArray.length == 2) {
                         var key = valArray[0].trim().toLowerCase();
                         secretTokens[key] = valArray[1].trim();
-                        console.log(`Secret token input found [${key}]`); 
+                        console.log(`Secret token input found [${key}]`);
                     }
                 }
             }
-            tl.debug(`secretTokens: found [${Object.keys(secretTokens).length}] tokens`);    
+            tl.debug(`secretTokens: found [${Object.keys(secretTokens).length}] tokens`);
         }
 
         tl.debug(`sourcePath: [${sourcePath}]`);
         tl.debug(`filePattern: [${filePattern}]`);
         tl.debug(`tokenRegex: [${tokenRegex}]`);
+        tl.debug(`useFindMethodL [${useFindMethod}]`);
 
-        if (!filePattern || filePattern.length === 0){
+        if (!filePattern || filePattern.length === 0) {
             filePattern = "*.*";
         }
         tl.debug(`Using [${filePattern}] as filePattern`);
@@ -45,13 +66,32 @@ async function run() {
         var separator = os.platform() === "win32" ? "\\" : "/";
 
         // create a glob removing any spurious quotes
-        var globPattern = `${sourcePath}${separator}${filePattern}`.replace(/\"/g,"");
+        var globPattern = `${sourcePath}${separator}${filePattern}`.replace(/\"/g, "");
         if (os.platform() !== "win32") {
-            // replace \ with /
             globPattern = globPattern.replace(/\\/g, "/");
         }
 
         var files = tl.glob(globPattern);
+
+        var filesRecursive = [];
+        if (useFindMethod) {
+            filesRecursive = GetAllFilesSync(`${sourcePath}`, filesRecursive);
+        }
+
+        if (filesRecursive && filesRecursive.length > 0) {
+            console.log(`Absolute files count found recursively is: [${filesRecursive.length}]`);
+            for (var i = 0; i < filesRecursive.length; i++) {
+                console.log(`File found: [${filesRecursive[i]}]`);
+
+                var lastIndex = filesRecursive[i].lastIndexOf(filePattern);
+                tl.warning(`Pattern Index is: [${lastIndex}]`);
+                if (lastIndex !== -1) {
+                    tl.warning(`File: [${filesRecursive[i]}] match pattern [${filePattern}]`);
+                    files.push(filesRecursive[i]);
+                }
+            }
+        }
+
         if (!files || files.length === 0) {
             var msg = `Could not find files with glob [${globPattern}].`;
             if (os.platform() !== "win32") {
@@ -62,16 +102,16 @@ async function run() {
         for (var i = 0; i < files.length; i++) {
             var file = files[i];
             console.info(`Starting regex replacement in [${file}]`);
-            
+
             var contents = fs.readFileSync(file, 'utf8').toString();
             var reg = new RegExp(tokenRegex, "g");
-                    
+
             // loop through each match
             var match: RegExpExecArray;
             // keep a separate var for the contents so that the regex index doesn't get messed up
             // by replacing items underneath it
             var newContents = contents;
-            while((match = reg.exec(contents)) !== null) {
+            while ((match = reg.exec(contents)) !== null) {
                 var vName = match[1];
                 if (typeof secretTokens[vName.toLowerCase()] !== 'undefined') {
                     // try find the variable in secret tokens input first
@@ -84,12 +124,12 @@ async function run() {
                         tl.warning(`Token [${vName}] does not have an environment value`);
                     } else {
                         newContents = newContents.replace(match[0], vValue);
-                        console.info(`Replaced token [${vName }]`);
-                    }           
+                        console.info(`Replaced token [${vName}]`);
+                    }
                 }
             }
             tl.debug("Writing new values to file...");
-            
+
             // make the file writable
             sh.chmod(666, file);
             fs.writeFileSync(file, newContents);

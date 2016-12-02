3423810da03ac30bd108752dfb6c911bbd3156d6
Colin Dembovsky <colindembovsky@gmail.com>
2016-12-01 22:50:14 -0600

    Adding junit report to tests

--- gulpfile.js
+++ gulpfile.js
@@ -3,44 +3,54 @@ var gulp = require('gulp');
 var path = require('path');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
-//require('babel-core/register');  // for mocha
 
 function reportErr(err){
     console.log('##vso[task.logissue type=error]' + err.message);
     console.log('##vso[task.complete result=failed]Failed');
 }
 
-gulp.task('test', function(cb) {
-    var tfBuild = false;
+// path to test files
+var testPaths = [
+    'test/instrumented/*.js',
+    'test/_testsuite.js'
+];
 
-    // path to test files
-    var testPaths = [
-        'test/instrumented/*.js',
-        'test/_testsuite.js'
-    ];
+// paths to src files for instrumentation
+var srcPaths = [
+    'Tasks/CoverageGate/*.js',
+    'Tasks/ReplaceTokens/*.js',
+    'Tasks/Tokenizer/*.js',
+    'Tasks/VersionAssemblies/*.js'
+];
 
-    // paths to src files for instrumentation
-    var srcPaths = [
-        'Tasks/CoverageGate/*.js',
-        'Tasks/ReplaceTokens/*.js',
-        'Tasks/Tokenizer/*.js',
-        'Tasks/VersionAssemblies/*.js'
-    ];
+var tfBuild = false;
 
-    // invoke the tests
-    gulp.src(srcPaths)
+gulp.task('instrument', function() {
+    return gulp.src(srcPaths)
         .pipe(istanbul())
         .pipe(gulp.dest('./test/instrumented'))
-        .on('finish', function() {
-            gulp.src(testPaths)
-                .pipe(mocha({ reporter: 'spec', ui: 'bdd', useColors: !tfBuild })
-                    .on('error', reportErr))
-                .pipe(istanbul.writeReports({
-                    includeAllSources: true,
-                    reporters: [ 'lcov', 'json', 'text', 'text-summary', 'html', 'cobertura' ]
-                }))
-                .on('error', reportErr)
-                .on('end', cb);
-        })
+        .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test-cover', ['instrument'], function() {
+    // invoke the tests
+    gulp.src(testPaths)
+        .pipe(mocha({ reporter: 'spec', ui: 'bdd', useColors: !tfBuild })
+            .on('error', reportErr))
+        .pipe(istanbul.writeReports({
+            includeAllSources: true,
+            reporters: [ 'lcov', 'json', 'text', 'text-summary', 'html', 'cobertura' ]
+        }))
         .on('error', reportErr);
+});
+
+gulp.task('test', ['instrument'], function() {
+    gulp.src(testPaths)
+        .pipe(mocha({ 
+            reporter:'mocha-junit-reporter',
+            reporterOptions: {
+                mochaFile: "./test-results.xml"
+            }
+        })
+            .on('error', reportErr));
 });
